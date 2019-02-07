43fdcf51d40b4eb818843c8a826a03f4a2869829
moniqueford <monique.ford@gmail.com>
2019-02-07 04:51:41 +0000

    print exactly using put value 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+/* write a C program that prints exactly using put function */
+#include <stdio.h>
+int main (void)
+{
+  char str[35]
+  puts("Programming is like a multilingual puzzle\n");
+  return (0);
+}
